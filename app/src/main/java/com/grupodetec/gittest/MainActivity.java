abaa62475a793fb6c5631f3b1f80aeb3d8846702
jose6palabras <jose6palabras@gmail.com>
2018-12-16 16:01:20 -0500

    second commit, changed two files.

--- app/src/main/java/com/grupodetec/gittest/MainActivity.java
+++ app/src/main/java/com/grupodetec/gittest/MainActivity.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public int casa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        casa = 2;
+
     }
 }
